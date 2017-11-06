5ff9b4494c32e108fa2b19087fe5c0aae9b9b9d0
sjt21027 <33235745+sjt21027@users.noreply.github.com>
2017-11-06 10:18:19 -0500

    Update env.cpp
    added doc comments

--- env.cpp
+++ env.cpp
@@ -1,13 +1,27 @@
+/*
+ * env.cpp 
+ *	
+ * The env utility prints all of the currently set
+ * environmental variables to standard output.
+ * 
+ * Owner: Steven Thompson - Gray Dunagan
+ * 
+ * Version: 11.6.17
+ */
+
 #include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
 /*
-This utility prints all of the currently set environmental
-variables
-*/
-
+ * This utility prints all of the currently set environmental
+ * variables
+ * Main function---
+ *
+ * @param argc the amount of arguments
+ * @param envp the amount of environment variables
+ */
 int main(int argc, char * argv[], char * envp[]){
 
   //for loop that enumerates and prints out each
