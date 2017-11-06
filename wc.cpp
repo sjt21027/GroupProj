aa5248bc01651b46c0420a72d693dd43c9db86b5
dgd45125 <dgd45125@uga.edu>
2017-11-06 09:58:13 -0500

    worked on wc -w

--- wc.cpp
+++ wc.cpp
@@ -113,10 +113,13 @@ int main(int argc, char* argv[]) {
 	   charCounter++;
 //          cout << "Character number: " << charCounter << " ---> " <<  buf[0] <<  endl;
 	 }
-	 if(charCounter > 1 && isspace(buf[0])){
+	 if(charCounter > 0 && z <= 0)
+	   words++;
+	 if(charCounter > 0 && (isspace(buf[0])     ) ){
 	   words++;
 	   charCounter = 0;
 	 }
+       
        }
 
        cout << words << " " << argv[i] << endl;
