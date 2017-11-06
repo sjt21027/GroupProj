2943913f4c25916858dcef5b0c63f03cdb361bb3
sjt21027 <33235745+sjt21027@users.noreply.github.com>
2017-11-06 10:23:41 -0500

    added doc comment
    added doc comments

--- false.cpp
+++ false.cpp
@@ -1,9 +1,21 @@
+/*
+ * false.cpp 
+ *	
+ * The false function returns either the function exits
+ * with success or not.
+ * 
+ * Owner: Steven Thompson - Gray Dunagan
+ * 
+ * Version: 11.6.17
+ */
+
 #include <cstdlib>
 
-/*This utility returns with exit code
-EXIT_FAILURE
-*/
-
+/* This utility returns with exit code
+ * EXIT_FAILURE
+ *
+ * Main function --
+ */
 int main(){
   return EXIT_FAILURE;
 }
