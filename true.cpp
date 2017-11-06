989be780958fb893e3d05d840f6bc44b0ad5a303
sjt21027 <33235745+sjt21027@users.noreply.github.com>
2017-11-06 10:31:47 -0500

    added comment
    added doc comments

--- true.cpp
+++ true.cpp
@@ -1,9 +1,22 @@
-#include <cstdlib>
 
 /*
-This utility returns with exit code EXIT_SUCCESS
-*/
+ * true.cpp 
+ *	
+ * The true function returns either the function exits
+ * with success or not.
+ * 
+ * Owner: Steven Thompson - Gray Dunagan
+ * 
+ * Version: 11.6.17
+ */
 
+#include <cstdlib>
+
+/* This utility returns with exit code
+ * EXIT_SUCCESS
+ *
+ * Main function --
+ */
 int main(){
   return EXIT_SUCCESS;
 }
